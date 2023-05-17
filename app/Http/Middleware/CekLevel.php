adf6cc60a8760fec06da4e55fc4d5bae362d35a8
oriza <oriza.200170194@mhs.unimal.ac.id>
2023-05-17 14:51:35 +0700

    ica

--- app/Http/Middleware/CekLevel.php
+++ app/Http/Middleware/CekLevel.php
@@ -16,7 +16,7 @@ class CekLevel
      */
     public function handle(Request $request, Closure $next, ...$levels)
     {
-        if (in_array($request->user()->leve,$levels)){
+        if (in_array($request->user()->level,$levels)){
             return $next($request);
         }
         return redirect('/');
